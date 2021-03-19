c1da3b328c88a4b5f46947653f411f79ee44a7fd
kimseokho582 <gjrehf@naver.com>
2021-03-20 08:55:12 +0900

    3-20
    1238

--- Project2/Project2/1238.cpp
+++ Project2/Project2/1238.cpp
@@ -2,13 +2,13 @@
 #include <vector>
 #include <queue>
 #include <cmath>
-#define INF 99999999
+#define INF 999999999
 using namespace std;
 
 vector<pair<int, int>>table[1001];
-vector<pair<int, int>>retable[1001];
 int dis[1001];
-int redis[1001];
+int N, M;
+bool check1=true,check2=true;
 void srh(int x) {
 	dis[x] = 0;
 	priority_queue<pair<int, int>>pq;
@@ -32,45 +32,56 @@ void srh(int x) {
 	}
 }
 
-void resrh(int x) {
-	redis[x] = 0;
-	priority_queue<pair<int, int>>pq;
-	pq.push({ 0,x }); //거리, 위치
-	while (!pq.empty()) {
-		int d = -pq.top().first;
-		int cur = pq.top().second;
-		pq.pop();
-
-		if (redis[cur] < d) continue;
-
-		for (int i = 0; i < retable[cur].size(); i++) {
-			int nd = d + retable[cur][i].second;
-			int next = retable[cur][i].first;
-
-			if (nd < redis[next]) {
-				redis[next] = nd;
-				pq.push({ -nd,next });
-			}
-		}
-	}
+void lnit() {
+	for (int i = 1; i <= N; i++) dis[i] = INF;
 }
 
 int main() {
-	int N, M, X; cin >> N >> M >> X;
-
-	for (int i = 1; i <= N; i++) dis[i] = redis[i] = INF;
+	cin >> N >> M ;
+	int tmp1 = 0;
+	int tmp2 = 0;
+	int ans;
+	
 	for (int i = 0; i < M; i++) {
 		int a, b, c; cin >> a >> b >> c;
 		table[a].push_back({ b,c });
-		retable[b].push_back({ a,c });
+		table[b].push_back({ a,c });
+		
 	}
 
-	srh(X);
-	resrh(X);
+	int x1, x2; cin >> x1 >> x2;
 
-	int ans = 0;
-	for (int i = 1; i <= N; i++) {
-		ans = max(ans, redis[i] + dis[i]);
+	lnit();
+	srh(1);
+	tmp1 += dis[x1];
+	tmp2 += dis[x2];
+	if (tmp1 == INF) check1 = false;
+	if (tmp2 == INF)check2 = false;
+
+	lnit();
+	srh(x1);
+	if (check1 == true) tmp1 += dis[x2];
+	if (check2 == true) tmp2 += dis[x2];
+	
+	lnit();
+	srh(x2); 
+	if (check1 == true) tmp1 += dis[N];
+
+	lnit();
+	srh(x1);
+	if (check2 == true) tmp2 += dis[N];
+
+	if (check1 == false && check2 == false) {
+		ans = -1;
 	}
-	cout << ans;
+	else {
+		ans = min(tmp1, tmp2);
+	}
+
+	if (ans == INF) {
+		cout << -1;
+	}
+	else {
+		cout << ans;
+	}
 }
