b22e1217c04dd0c61e0b4366c5b43f43de4110b6
kimseokho582 <gjrehf@naver.com>
2021-04-01 01:01:33 +0900

    4-1
    11057

--- Project2/Project2/11057.cpp
+++ Project2/Project2/11057.cpp
@@ -1,34 +1,20 @@
 #include <iostream>
-#include<cmath>
 using namespace std;
 
-#define MAX_SIZE 1001
-int table[MAX_SIZE] = { 0, };
-int ccount = 0;
-void calc(int n) {
-	for (int i = 0; i < pow(10, n); i++) {
-		int flag = 0;
-		for (int j = 1; j <= n; j++) {
-			int num = i;
-			num = num/pow(10, j-1);
-			table[n-j] = num % 10;
-			
-		}
-		for (int j = 0; j < n-1; j++) {
-			if (table[j] > table[j + 1]) {
-				break;
-			}
-			else
-				flag++;
-		}
-		if (flag == n - 1)
-			ccount++;
-	}
-
-}
-
 int main() {
-	int n; cin >> n;
-	calc(n);
-	cout<<ccount;
+	long long table[2][10];
+	long long N; cin >> N;
+	
+	for (int i = 0; i < 10; i++) {
+		table[0][i] = i+1;
+	}
+	table[1][0] = 1;
+
+	for (int i = 1; i < N ; i++) {
+		for (int j = 1; j < 10; j++) {
+			i % 2 == 1 ? table[1][j] = (table[1][j - 1] + table[0][j]) % 10007 : table[0][j] = (table[0][j - 1] + table[1][j]) % 10007;
+		}
+	}
+	N%2==1? cout << table[0][9] % 10007: cout << table[1][9] % 10007;
 }
+
