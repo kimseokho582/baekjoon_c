1c7c3c72ba80f76d26f66bbecaab794e652c0510
kimseokho582 <gjrehf@naver.com>
2021-04-05 23:00:34 +0900

    4-5
    11722

---
+++ Project2/Project2/11722.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	int n; cin >> n; int ans = 0;
+	vector<pair<int, int>>table(n);
+	cin >> table[0].first; table[0].second = 1;
+	for (int i = 1; i < n; i++) {
+		cin >> table[i].first; table[i].second = 1;
+		for (int j = 0; j < i; j++) {
+			if (table[i].first < table[j].first && table[i].second < table[j].second + 1) {
+				table[i].second = table[j].second + 1;
+			}
+
+		}
+
+	}
+
+	for (int i = 0; i < n; i++) ans = max(ans, table[i].second);
+	cout << ans;
+
+}
