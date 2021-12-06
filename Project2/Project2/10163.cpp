7885df98d7670ed7b92a1dc5db5e9c48b34dd8d9
kimseokho582 <73897413+kimseokho582@users.noreply.github.com>
2021-12-06 13:11:13 +0900

    12-06
    10163

--- Project2/Project2/10163.cpp
+++ Project2/Project2/10163.cpp
@@ -1,40 +1,32 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
-int ssize[1001] = { 0, };
-int table[101][101] = { 0, };
-int coordi[1001][4];
-
 int main() {
-	int N; cin >> N;
+    vector<vector <int>> v(101, vector<int>(101, 0));
+    int n;
+    scanf_s("%d", &n);
 
-	for (int i = 1; i <= N; i++) {
-		for (int j = 0; j < 4; j++) {
-			cin >> coordi[i][j];
-		}
-	}
-	
+    for (int i = 1; i <= n; i++) {
+        int x, y, lenx, leny;
+        scanf_s("%d %d %d %d", &x, &y, &lenx, &leny);
+        for (int j = y; j < y + leny; j++) {
+            for (int k = x; k < x + lenx; k++) {
+                v[k][j] = i;
+            }
+        }
+    }
+    for (int i = 1; i <= n; i++) {
+        int count = 0;
 
+        for (int j = 0; j <= 100; j++) {
+            for (int k = 0; k <= 100; k++) {
+                if (v[j][k] == i)
+                    count++;
+            }
+        }
 
-	for (int i = 1; i <= N; i++) {
-		for (int j = coordi[i][0]; j < coordi[i][0]+coordi[i][2]; j++) {
-			for (int k = coordi[i][1]; k < coordi[i][1]+coordi[i][3]; k++) {
-				table[j][k] = i;
-			}
-		}
-	}
-
-	for (int i = 1; i <= N; i++) {
-		for (int j = 0; j < 101; j++) {
-			for (int k = 0; k < 101; k++) {
-				if (i == table[j][k]) {
-					ssize[i]++;
-				}
-			}
-		}
-	}
-
-	for (int i = 1; i <= N; i++)
-		cout << ssize[i] << endl;
+        printf("%d\n", count);
+    }
+    return 0;
 }
