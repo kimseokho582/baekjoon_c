4685560f8a4a141eccdc63890a00b8ba8272b6e1
kimseokho582 <73897413+kimseokho582@users.noreply.github.com>
2021-12-31 11:01:55 +0900

    12-31
    17626

--- Project2/Project2/17626.cpp
+++ Project2/Project2/17626.cpp
@@ -1,22 +1,19 @@
-#include<iostream>
-#include<algorithm>
+#include <iostream>
+#include <algorithm>
 using namespace std;
 
-int n;
-int dp[50001];
+int table[50001];
 
-int main()
-{
-	cin >> n;
-	dp[1] = 1;
-	for (int i = 1; i <= n; i++)
-	{
-		dp[i] = dp[1] + dp[i - 1];
-		for (int j = 2; j * j <= i; j++)
-		{
-			dp[i] = min(dp[i], 1 + dp[i - j * j]);
+int main() {
+	int n; cin >> n;
+	table[0] = 0;
+	table[1] = 1;
+	for (int i = 2; i <= n; i++) {
+		int tmp = 50001;
+		for (int j = 1; j*j <= i; j++) {
+			tmp = min(tmp, table[i - j * j] + 1);
 		}
+		table[i] = tmp;
 	}
-
-	cout << dp[n];
+	cout << table[n];
 }
