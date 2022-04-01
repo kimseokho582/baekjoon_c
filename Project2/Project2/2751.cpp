7a39d2eeb8031231d78e433a126586a3146c9fff
kimseokho582 <73897413+kimseokho582@users.noreply.github.com>
2022-04-01 11:34:29 +0900

    4-1
    2751

--- Project2/Project2/2751.cpp
+++ Project2/Project2/2751.cpp
@@ -1,70 +1,17 @@
 #include <iostream>
-
+#include <algorithm>
+#include <vector>
 using namespace std;
 
-int N;
-int table[1000001];
-int* ans;
-
-void merge(int left, int mid, int right) {
-    int i = left;
-    int j = mid + 1;
-    int k = left;
-
-    while (i <= mid && j <= right) {
-        if (table[i] <= table[j]) {
-            ans[k++] = table[i++];
-
-        }
-        else
-        {
-            ans[k++] = table[j++];
-
-        }
-
-    }
-    if (i > mid) {
-        for (int q = j; q <= right; q++) {
-            ans[k] = table[q];
-            k++;
-        }
-    }
-    else {
-        for (int q = i; q <= mid; q++) {
-            ans[k] = table[q];
-            k++;
-        }
-    }
-    for (int i = left; i <= right; i++)
-        table[i] = ans[i];
-}
-
-
-void mergeSort(int left, int right) {
-    int mid;
-
-    if (left < right) {
-        mid = (left + right) / 2;
-        mergeSort(left, mid);
-        mergeSort(mid + 1, right);
-        merge(left, mid, right);
-    }
-
-
-}
+vector<int>v;
 
 int main() {
-    //cin >> N;
-    scanf("%d", &N);
-    ans = new int[N];
-    for (int i = 0; i < N; i++) {
-        cin >> table[i];
-    }
-
-    mergeSort(0, N - 1);
-
-
-    for (int i = 0; i < N; i++)
-        cout << table[i] << " ";
-
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int n; cin >> n;
+	for (int i = 0; i < n; i++) {
+		int tmp; cin >> tmp;
+		v.push_back(tmp);
+	}
+	stable_sort(v.begin(), v.end());
+	for (int x : v)cout << x << "\n";
 }
